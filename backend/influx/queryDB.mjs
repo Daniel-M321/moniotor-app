7ab1a7c3980c24b1cf50cffc19b18da111b5bac5
Daniel-M321 <millettdanny5@gmail.com>
2023-02-07 17:56:30 +0000

    changed how data is queried from DB

--- backend/influx/queryDB.mjs
+++ backend/influx/queryDB.mjs
@@ -3,29 +3,51 @@ import {url, token, org} from '../env.mjs'
 
 const queryApi = new InfluxDB({url, token}).getQueryApi(org)
 
-async function queryTime() {
+async function queryTime(req_params) {
   console.log('*** QUERY ROWS ***')
-
+  console.log(req_params) //todo no values in this
   const start = fluxDuration('-30d')
-  const measurement = 'myMeasurement'
+  var measurement = "null"
+
+  if(req_params.measurement)
+    measurement = req_params.measurement
 
   const fluxQuery = flux`from(bucket:"sensors") 
-    |> range(start: ${start}) 
+    |> range(start: ${start}, stop: now()) 
     |> filter(fn: (r) => r._measurement == ${measurement})`
   console.log('query:', fluxQuery.toString())
 
-  try {
-    for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
-      const o = tableMeta.toObject(values)
-      //console.log(JSON.stringify(o, null, 2))
-      console.log(
-        `${o._time} ${o._measurement} in '${o.location}' (${o.example}): ${o._field}=${o._value}`
-      )
-      return o
-    }
-  } catch (e) {
-    console.log('\nFinished ERROR: '+e)
-  }
+//   try {
+//     for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
+//       const o = tableMeta.toObject(values)
+//       //console.log(JSON.stringify(o, null, 2))
+//       console.log(
+//         `${o._time} ${o._measurement} in '${o.location}' (${o.example}): ${o._field}=${o._value}`
+//       )
+//       return o
+//     }
+//   } catch (e) {
+//     console.log('\nFinished ERROR: '+e)
+//   }
+// }
+
+  const scatterData = [];
+  const lineBarData = {};
+  await queryApi
+      .collectRows(fluxQuery /*, you can specify a row mapper as a second arg */)
+      .then(data => {
+          data.forEach((x) => {
+              scatterData.push({ x: x._time, y: x._value });
+              lineBarData[x._time] = new Date(x._value).getTime()
+          })
+          //console.log('\nCollect ROWS SUCCESS')
+      })
+      .catch(error => {
+          console.error(error)
+          console.log('\nCollect ROWS ERROR')
+      })
+  return ({ scatterData, lineBarData })
+
 }
 
 export {queryTime}
