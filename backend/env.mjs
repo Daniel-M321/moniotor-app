8070b335c0698edd9f47b57eef2744edfee7b8c1
Daniel-M321 <millettdanny5@gmail.com>
2023-02-22 20:26:18 +0000

    added buildspec

--- backend/env.mjs
+++ backend/env.mjs
@@ -1,14 +1,14 @@
 /** InfluxDB v2 URL */
-const url = process.env['INFLUX_URL']
+const url = process.env['INFLUX_URL'] || 'http://moniotor-influxdb-1:8086'
 /** InfluxDB authorization token */
 const token = process.env['DOCKER_INFLUXDB_INIT_ADMIN_TOKEN']
 /** Organization within InfluxDB  */
-const org = process.env['DOCKER_INFLUXDB_INIT_ORG']
+const org = process.env['DOCKER_INFLUXDB_INIT_ORG'] || 'nuig'
 /**InfluxDB bucket used in examples  */
-const bucket = process.env['DOCKER_INFLUXDB_INIT_BUCKET']
+const bucket = process.env['DOCKER_INFLUXDB_INIT_BUCKET'] || 'sensors'
 // ONLY onboarding example
 /**InfluxDB user  */
-const username = process.env['DOCKER_INFLUXDB_INIT_USERNAME']
+const username = process.env['DOCKER_INFLUXDB_INIT_USERNAME'] || 'admin'
 /**InfluxDB password  */
 const password = process.env['DOCKER_INFLUXDB_INIT_PASSWORD']
 
